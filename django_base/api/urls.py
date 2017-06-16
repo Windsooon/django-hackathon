2654c245cf3e6d60ce4555316285b97434c8443a
Windson <wiwindson@outlook.com>
2017-06-16 15:15:56 +0800

    use viewset instead

--- django_base/api/urls.py
+++ django_base/api/urls.py
@@ -2,7 +2,34 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
+inner_list = views.InnerViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+inner_detail = views.InnerViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+playlist_list = views.PlaylistViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+playlist_detail = views.PlaylistViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
 urlpatterns = format_suffix_patterns([
-    url(r'inner/$', views.InnerList.as_view()),
-    url(r'inner/(?P<pk>[0-9]+)/$', views.InnerDetail.as_view()),
+    url(r'inner/$', inner_list, name='inner_list'),
+    url(r'inner/(?P<pk>[0-9]+)/$', inner_detail, name='inner_detail'),
+    url(r'playlist/$', playlist_list, name='playlist_list'),
+    url(r'playlist/(?P<pk>[0-9]+)/$', playlist_detail, name='playlist_detail'),
+    url(r'$', views.api_root),
 ])
--- django_base/api/views.py
+++ django_base/api/views.py
@@ -1,13 +1,27 @@
-from inner.models import Inner
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from inner.serializers import InnerSerializer
-from rest_framework import generics
+from inner.models import Inner
+from playlist.models import Playlist
+from playlist.serializers import PlaylistSerializer
+# from .permissions import IsAdminOrReadOnly
 
 
-class InnerList(generics.ListCreateAPIView):
+class InnerViewSet(viewsets.ModelViewSet):
     queryset = Inner.objects.all()
     serializer_class = InnerSerializer
 
 
-class InnerDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Inner.objects.all()
-    serializer_class = InnerSerializer
+class PlaylistViewSet(viewsets.ModelViewSet):
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'inner': reverse('inner_list', request=request, format=format),
+        'playlist': reverse('playlist_list', request=request, format=format)
+    })
